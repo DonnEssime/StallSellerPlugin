cff5d5bd507e13c309b63ddca4da7e64b2e472a4
Simon Donné <donnesimon@gmail.com>
2016-12-03 12:05:49 +0100

    updated the plugin to not block anymore

--- src/haven/plugins/StallSellerPlugin.java
+++ src/haven/plugins/StallSellerPlugin.java
@@ -5,38 +5,44 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class StallSellerPlugin extends Plugin{
-    public void load(UI ui)
-    {
+public class StallSellerPlugin extends Plugin {
+
+    public void load(UI ui) {
         Glob glob = ui.sess.glob;
         Collection<Glob.Pagina> p = glob.paginae;
         p.add(glob.paginafor(Resource.load("paginae/add/stallseller")));
-        XTendedPaginae.registerPlugin("stallseller",this);
+        XTendedPaginae.registerPlugin("stallseller", this);
     }
-    
-    public void execute(UI ui){
-        ui.message("[StallSeller] Selling items to stall. This may take a while.", GameUI.MsgType.INFO);
-        
-        try {
-            Thread.sleep(150);
-        } catch (InterruptedException ex) {
-        }
-        
-        List<WItem> ws = new ArrayList<WItem>();
-        for (Widget wdg = UI.instance.gui.maininv.lchild; wdg != null; wdg = wdg.prev) {
-            if (wdg.visible && wdg instanceof WItem) {
-                ws.add(((WItem) wdg));
+
+    public void execute(final UI ui) {
+        ui.message("[StallSeller] Selling items to stall. Please wait until further notice.", GameUI.MsgType.INFO);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<WItem> items = ui.gui.maininv.getSameName("", true);
+                for (int i = items.size() - 1; i < items.size() && i >= 0; i--) {
+
+                    Resource curs = ui.root.cursor;
+                    if (!curs.name.contains("kreuz")) {
+                        ui.message("[StallSeller] You are not selling to a stall! Aborting.", GameUI.MsgType.BAD);
+                        return;
+                    }
+
+                    WItem subject = items.get(i);
+                    subject.mousedown(Coord.z, 1);
+
+                    try {
+                        Thread.sleep(150);
+                    } catch (InterruptedException ex) {
+                    }
+
+                    items = ui.gui.maininv.getSameName("", true);
+                }
+                ui.message("[StallSeller] Stopped selling items.", GameUI.MsgType.INFO);
             }
-        }
-        for(WItem w : ws)
-        {
-            w.mousedown(Coord.z, 1);
-            try {
-                Thread.sleep(150);
-            } catch (InterruptedException ex) {
-            }
-        }
-        
-        ui.message("[StallSeller] Finished selling items!", GameUI.MsgType.INFO);
+        },
+                 "Stall seller").start();
+
     }
 }
